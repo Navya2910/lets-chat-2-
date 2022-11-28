8593b6259456d94d673e60adf0a8686885a16ea4
Navya2910 <115222583+Navya2910@users.noreply.github.com>
2022-11-28 16:01:26 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD3lx4mey4U9S3AqKy_2xxQ_ESOBx8d-8A",
+    authDomain: "lets-chat-bb9cc.firebaseapp.com",
+    databaseURL: "https://lets-chat-bb9cc-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-bb9cc",
+    storageBucket: "lets-chat-bb9cc.appspot.com",
+    messagingSenderId: "1044555234345",
+    appId: "1:1044555234345:web:6b7455a6e45fdac025b038"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
